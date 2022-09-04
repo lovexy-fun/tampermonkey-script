64a652947fe02c7ddd9db49647727a804c6ede8c
lovexy-fun <lovexyfun@qq.com>
2022-09-05 02:02:51 +0800

    更新界面优化和复制问题

--- src/csdn-optimizer.user.js
+++ src/csdn-optimizer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        CSDN博客优化
 // @namespace   http://lovexy.fun/
-// @version     0.0.1
+// @version     1.0.0
 // @description 优化CSDN博客展示
 // @author      lovexy-fun
 // @match       https://blog.csdn.net/*
@@ -16,7 +16,39 @@
     'use strict';
 
     /* 解除复制限制 */
-    $("#content_views pre").css("user-select", "text");
-    $("#content_views pre code").css("user-select", "text");
+    try {
+        $("#content_views pre").css("user-select", "text");
+        $("#content_views pre code").css("user-select", "text");
+    } catch (error) {
+        console.error({msg: "解除复制限制错误", err: error});
+    }
+    
+
+    /* 优化界面 */
+    try {
+        $("#csdn-toolbar").remove();//移除顶栏
+        $(".blog_container_aside").remove();//移除左侧边栏
+        $("#rightAside").remove();//移除右侧边栏
+        $("#rightAsideConcision").remove();//移除右侧边栏
+        $(".recommend-box").remove();//移除推荐文章
+        $("#recommendNps").remove();//移除相关推荐评价
+        $(".template-box").remove();//移除主题介绍
+        $("#tool-share").remove();//移除分享
+        $(".csdn-side-toolbar ").remove();//移除侧边工具按钮
+        $("main").width("100%");//放大主体文章部分
+        $("body").css('background', 'rgb(245,246,247)');//修改背景色
+    } catch (error) {
+        console.error({msg: "优化界面错误", err: error});
+    }
+    
+    /* 去掉复制版权尾巴 */
+    try {
+        [...document.getElementsByTagName("article")].forEach( i => {
+            i.oncopy = e => e.stopPropagation();
+            }
+        );
+    } catch (error) {
+        console.error({msg: "去掉复制版权尾巴错误", err: error});
+    }
 
 })();
